57a741b5e172a6acc30f38d4a00cdb149e05764c
Javier González de la Barreda Arimany <72403194+Javigba@users.noreply.github.com>
2023-12-16 11:30:11 +0000

    añadiendo etiquetas doxygen Passenger.java

--- src/main/java/es/ull/passengers/Passenger.java
+++ src/main/java/es/ull/passengers/Passenger.java
@@ -1,3 +1,8 @@
+/**
+ * @file Passenger.java
+ * @brief Contiene la implementación de la clase Passenger.
+ */
+
 /*
  * ========================================================================
  *
@@ -18,6 +23,7 @@
  *
  * ========================================================================
  */
+
 package es.ull.passengers;
 
 import java.util.Arrays;
@@ -27,13 +33,23 @@ import java.util.regex.Pattern;
 
 import es.ull.flights.Flight;
 
+/**
+ * @brief Clase que representa a un pasajero y gestiona la información asociada.
+ */
 public class Passenger {
 
-    private String identifier;
-    private String name;
-    private String countryCode;
-    private Flight flight;
+    private String identifier; /**< Identificador único del pasajero. */
+    private String name; /**< Nombre del pasajero. */
+    private String countryCode; /**< Código del país del pasajero. */
+    private Flight flight; /**< Vuelo al que está asociado el pasajero, puede ser nulo. */
 
+    /**
+     * @brief Constructor de la clase Passenger.
+     * @param identifier Identificador único del pasajero.
+     * @param name Nombre del pasajero.
+     * @param countryCode Código del país del pasajero.
+     * @throws RuntimeException Si el código de país no es válido.
+     */
     public Passenger(String identifier, String name, String countryCode) {
         if (!Arrays.asList(Locale.getISOCountries()).contains(countryCode)) {
             throw new RuntimeException("Invalid country code");
@@ -44,22 +60,42 @@ public class Passenger {
         this.countryCode = countryCode;
     }
 
+    /**
+     * @brief Obtiene el identificador único del pasajero.
+     * @return Identificador único del pasajero.
+     */
     public String getIdentifier() {
         return identifier;
     }
 
+    /**
+     * @brief Obtiene el nombre del pasajero.
+     * @return Nombre del pasajero.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @brief Obtiene el código del país del pasajero.
+     * @return Código del país del pasajero.
+     */
     public String getCountryCode() {
         return countryCode;
     }
 
+    /**
+     * @brief Obtiene el vuelo al que está asociado el pasajero.
+     * @return Vuelo al que está asociado el pasajero, puede ser nulo.
+     */
     public Flight getFlight() {
         return flight;
     }
 
+    /**
+     * @brief Asocia al pasajero con un vuelo y actualiza la información.
+     * @param flight Vuelo al que se une el pasajero.
+     */
     public void joinFlight(Flight flight) {
         Flight previousFlight = this.flight;
         if (null != previousFlight) {
@@ -75,12 +111,21 @@ public class Passenger {
         }
     }
 
+    /**
+     * @brief Establece el vuelo asociado al pasajero.
+     * @param flight Nuevo vuelo asociado al pasajero.
+     */
     public void setFlight(Flight flight) {
         this.flight = flight;
     }
 
+    /**
+     * @brief Representación en cadena del objeto Passenger.
+     * @return Representación en cadena del objeto Passenger.
+     */
     @Override
     public String toString() {
         return "Passenger " + getName() + " with identifier: " + getIdentifier() + " from " + getCountryCode();
     }
 }
+
