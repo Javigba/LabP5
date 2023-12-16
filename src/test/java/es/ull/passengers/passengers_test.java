2d0c2060dae3542f00e2b63b45065804f348a2ac
Javier González de la Barreda Arimany <72403194+Javigba@users.noreply.github.com>
2023-12-16 10:38:15 +0000

    Update passengers_test.java

--- src/test/java/es/ull/passengers/passengers_test.java
+++ src/test/java/es/ull/passengers/passengers_test.java
@@ -1,8 +1,3 @@
-/**
- * @file PassengerTest.java
- * @brief This file contains the unit tests for the Passenger class.
- */
-
 package es.ull.passengers;
 import es.ull.passengers.Passenger;
 import es.ull.flights.Flight;
@@ -10,56 +5,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 
-/**
- * @brief Test class for the Passenger class.
- */
+
+
 class PassengerTest {
 
-    private Passenger passenger; /**< Passenger object for testing. */
+    private Passenger passenger;
 
-    /**
-     * @brief Set up the test environment before each test case.
-     */
     @BeforeEach
     void setUp() {
         passenger = new Passenger("ID001", "John Doe", "US");
     }
 
-    /**
-     * @brief Test for getting the passenger identifier.
-     */
     @Test
     void testGetIdentifier() {
         assertEquals("ID001", passenger.getIdentifier());
     }
 
-    /**
-     * @brief Test for getting the passenger name.
-     */
     @Test
     void testGetName() {
         assertEquals("John Doe", passenger.getName());
     }
 
-    /**
-     * @brief Test for getting the passenger country code.
-     */
     @Test
     void testGetCountryCode() {
         assertEquals("US", passenger.getCountryCode());
     }
 
-    /**
-     * @brief Test for getting the passenger's associated flight.
-     */
     @Test
     void testGetFlight() {
         assertNull(passenger.getFlight());
     }
 
-    /**
-     * @brief Test for joining a flight.
-     */
     @Test
     void testJoinFlight() {
         Flight flight = new Flight("AB123", 100);
@@ -69,23 +45,16 @@ class PassengerTest {
         assertEquals(1, flight.getNumberOfPassengers());
     }
 
-    /**
-     * @brief Test for leaving a flight.
-     */
     @Test
     void testLeaveFlight() {
         Flight flight = new Flight("AB123", 100);
         passenger.joinFlight(flight);
         passenger.joinFlight(null);
-        passenger.leaveFlight();
 
         assertNull(passenger.getFlight());
         assertEquals(0, flight.getNumberOfPassengers());
     }
 
-    /**
-     * @brief Test for creating a passenger with an invalid country code.
-     */
     @Test
     void testInvalidCountryCode() {
         assertThrows(RuntimeException.class, () -> new Passenger("ID001", "John Doe", "XX"));
